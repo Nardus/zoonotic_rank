19aba50f5f45b3e5683afb6b48997daec2c9a2d1
Nardus Mollentze <nmollentze@gmail.com>
2026-04-15 14:35:29 +0100

    Replace wget with curl for downloading models to ensure macOS compatibility

--- Scripts/DownloadTrainedModels.sh
+++ Scripts/DownloadTrainedModels.sh
@@ -2,7 +2,7 @@
 
 # Download pretrained models and associated data required for predictions from Zenodo
 
-wget -O zenodo_archive.zip https://zenodo.org/records/5155330/files/Nardus/zoonotic_rank-v1.2.1.zip?download=1
+curl -L -o zenodo_archive.zip "https://zenodo.org/records/5155330/files/Nardus/zoonotic_rank-v1.2.1.zip?download=1"
 
 unzip -jo zenodo_archive.zip "Nardus-zoonotic_rank-25ef305/CalculatedData/*" -d "CalculatedData/"
 unzip -jo zenodo_archive.zip "Nardus-zoonotic_rank-25ef305/RunData/PN_LongRun/*" -d "RunData/PN_LongRun/"
